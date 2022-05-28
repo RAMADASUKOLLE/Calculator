05a8550e33d75b195077bd2bec362ab82277b894
HARI <RAMADASU.neo@gmail.com>
2022-05-28 22:52:15 +0530

    new code for Substraction

--- Addition.java
+++ Addition.java
@@ -3,5 +3,6 @@ class Addition
 	public static void main(String args[])
 	{
 		System.out.println("Addition of numbers is : "+(10+5));
+		System.out.println("Substraction of numbers is : "+(10-5));
 	}
 }
